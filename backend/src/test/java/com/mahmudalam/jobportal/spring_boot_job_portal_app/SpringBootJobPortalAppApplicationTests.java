13555562c31f5a0d07e8e0382b63f3b140d12327
suvajitadhal12 <suvajitadhal12@gmail.com>
2025-10-04 11:59:52 +0530

    Add files via upload

---
+++ backend/src/test/java/com/mahmudalam/jobportal/spring_boot_job_portal_app/SpringBootJobPortalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmudalam.jobportal.spring_boot_job_portal_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJobPortalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
